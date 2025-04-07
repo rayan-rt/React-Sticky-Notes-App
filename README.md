8e9e26f53b96f35cb8566e60d84aaf753052a417
rayan-rt <tanzeemrayan2@gmail.com>
2025-04-07 16:28:06 +0500

    Modify readme.md

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# React + Vite
+# React Sticky Notes App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a simple React app that allows users to create sticky notes and organize them into categories by their similar colors. The app uses local storage to store data, Context API to manage state.
 
-Currently, two official plugins are available:
+### Technologies Used
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- React
+- Context API
+- Local Storage
+- React-Icons
+- Tailwind CSS
+
+### Demo
+
+[![Watch Demo](https://img.youtube.com/vi/Zgsf-bnOUkE/0.jpg)](https://www.youtube.com/watch?v=Zgsf-bnOUkE)
--- src/Components/NoteContainer.jsx
+++ src/Components/NoteContainer.jsx
@@ -2,21 +2,21 @@ import { useNoteContext } from "../Contexts/NoteContext";
 import NoteBox from "./NoteBox";
 
 const NoteContainer = () => {
-	// context
-	let { notes } = useNoteContext();
+  // context
+  let { notes } = useNoteContext();
 
-	return (
-		<div className="w-full h-full">
-			<h1 className="pt-4 text-xl text-center font-semibold text-blue-500">
-				Your Notes
-			</h1>
-			<div className="mt-4 w-full h-full p-2 grid grid-cols-4 gap-4">
-				{notes.map((note) => (
-					<NoteBox key={note.id} note={note} />
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-full">
+      <h1 className="pt-4 text-xl md:text-3xl text-center font-semibold text-blue-500">
+        Your Notes
+      </h1>
+      <div className="mt-4 w-full h-full p-2 grid grid-cols-4 gap-4">
+        {notes.map((note) => (
+          <NoteBox key={note.id} note={note} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default NoteContainer;
